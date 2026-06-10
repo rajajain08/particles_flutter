534ee30e1aa5df9ef775857eda9c8c0eaf72d300
rajajain08 <rajajain08@gmail.com>
2026-06-10 23:20:27 +0530

    integrated vercel build

--- scripts/build_flutter_web.sh
+++ scripts/build_flutter_web.sh
@@ -3,15 +3,16 @@ set -e
 
 export FLUTTER_HOME="$HOME/flutter"
 export PATH="$FLUTTER_HOME/bin:$PATH"
+export PUB_CACHE="$HOME/.pub-cache"
 
 if [ ! -d "$FLUTTER_HOME" ]; then
   git clone https://github.com/flutter/flutter.git --depth 1 --branch 3.24.0 "$FLUTTER_HOME"
 fi
 
+flutter config --enable-web --no-analytics
 flutter precache --web
-flutter config --enable-web
-flutter --version
 
 cd particles_flutter/example
+rm -f pubspec.lock
 flutter pub get
-flutter build web --release
+flutter build web --release --no-tree-shake-icons
